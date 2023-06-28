3032f45b69a32596734787d707e5848183b2b8c8
Vikash Patel <vpatel884@gmail.com>
2023-06-28 23:27:02 +0530

    Operators in JS

---
+++ jsOperators.js
@@ -0,0 +1,30 @@
+const sum = 12 + 11;
+const difference = 12 - 11;
+const product = 12 * 11;
+const quotient = 12/12;
+let myVar = 22;
+myVar++;
+// Increment operator
+myVar--;
+//Decrement operator
+const remainder = 11 % 3;
+// Used to find remainder
+
+let a = 12;
+let b = 13;
+let c = 14;
+// Augmented Addition
+a += 8;
+// = 20
+b += 7;
+// = 20
+c += 6;
+// = 20
+
+//Augmented Substraction
+a -= 2;
+//= 10
+b -= 3;
+//= 10
+c -= 4;
+// = 10

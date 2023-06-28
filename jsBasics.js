d93f0dec6f569ace5054a0bf3aa2509c7b61d455
Vikash Patel <vpatel884@gmail.com>
2023-06-28 22:54:16 +0530

    Variables in JS

--- jsBasics.js
+++ jsBasics.js
@@ -1,3 +1,22 @@
 // Single line comment in javascript
 /* Multi line comments
 in javascript*/
+
+var a;
+// a is undefined variable
+a = 5;
+// Also can be written as
+
+var b = 7;
+var c = b;
+// Value of a variable can be given to another varible
+
+var d = "New variable";
+// d is String variable
+
+var myName = "Vikash Patel";
+//variable name is case sensitive and mainly written in camelCase 
+
+const myName = "Vikash";
+//variables declared using const are read-only
+let myLastName = "Patel";

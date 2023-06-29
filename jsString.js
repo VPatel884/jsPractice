d005c6c261a148d1620f51d26d163f134f2dd62b
Vikash Patel <vpatel884@gmail.com>
2023-06-29 23:11:04 +0530

    Escape sequence in strings

---
+++ jsString.js
@@ -0,0 +1,11 @@
+// Escape Sequences
+let myStr = "FirstLine\n\t\SecondLine\nThirdLine"
+/* Code	Output
+    \'	single quote
+    \"	double quote
+    \\	backslash
+    \n	newline
+    \t	tab
+    \r	carriage return
+    \b	backspace
+    \f	form feed */

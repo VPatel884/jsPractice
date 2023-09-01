6318092bb748f67d544ad97b0216a2cf55659e2e
Vikash Patel <vpatel884@gmail.com>
2023-09-01 18:31:02 +0530

    Created function main().

--- calculator.js
+++ calculator.js
@@ -18,3 +18,35 @@ function toDivide(num1,num2) {
     }
     return num1/num2;
 }
+
+function main() {
+    let num1 = readlineSync.question("Enter the first number: ")
+    let operation = readlineSync.question("Choose an operator (+, -, *, /): ")
+    let num2 = readlineSync.question("Enter the second number: ")
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if (isNaN(num1) || isNaN(num2)) {
+        console.log("Entered an invalid number. Please enter valid number.")
+    } 
+
+    let result;
+    switch (operation) {
+        case '+':
+            result = add(num1, num2);
+            break;
+        case '-':
+            result = subtract(num1, num2);
+            break;
+        case '*':
+            result = multiply(num1, num2);
+            break;
+        case '/':
+            result = divide(num1, num2);
+            break;
+        default:
+            console.log('Invalid operation. Please choose +, -, *, or /.');
+    }
+
+}
+
+main()
